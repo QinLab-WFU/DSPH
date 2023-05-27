e0eb5dabf0abd85e751426a49a55775e7ee6604c
QinLab-WFU <128210944+QinLab-WFU@users.noreply.github.com>
2023-05-27 21:04:00 +0800

    Add files via upload

---
+++ map.py
@@ -0,0 +1,37 @@
+import h5py
+import scipy.io as scio
+import numpy as np
+import torch
+
+
+def calc_hammingDist(B1, B2):
+    q = B2.shape[1]
+    if len(B1.shape) < 2:
+        B1 = B1.unsqueeze(0)
+    distH = 0.5 * (q - B1.mm(B2.transpose(0, 1)))
+    return distH
+
+
+def calc_map_k_matrix(qB, rB, query_L, retrieval_L, k=None, rank=0):
+    num_query = query_L.shape[0]
+    if qB.is_cuda:
+        qB = qB.cpu()
+        rB = rB.cpu()
+    map = 0
+    if k is None:
+        k = retrieval_L.shape[0]
+    for iter in range(num_query):
+        gnd = (query_L[iter].unsqueeze(0).mm(retrieval_L.t()) > 0).type(torch.float).squeeze()
+        tsum = torch.sum(gnd)
+        if tsum == 0:
+            continue
+        hamm = calc_hammingDist(qB[iter, :], rB)
+        _, ind = torch.sort(hamm)
+        ind.squeeze_()
+        gnd = gnd[ind]
+        total = min(k, int(tsum))
+        count = torch.arange(1, total + 1).type(torch.float).to(gnd.device)
+        tindex = torch.nonzero(gnd)[:total].squeeze().type(torch.float) + 1.0
+        map += torch.mean(count / tindex)
+    map = map / num_query
+    return map
